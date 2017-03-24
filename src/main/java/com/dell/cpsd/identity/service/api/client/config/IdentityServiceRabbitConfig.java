c820c8900ff9696c0a170ff780616ba1a2426d48
gouldc <connor.goulding@dell.com>
2017-03-24 14:29:51 +0000

    US52362 - Change file read method

--- src/main/java/com/dell/cpsd/identity/service/api/client/config/IdentityServiceRabbitConfig.java
+++ src/main/java/com/dell/cpsd/identity/service/api/client/config/IdentityServiceRabbitConfig.java
@@ -30,12 +30,8 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.util.FileSystemUtils;
 
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
 import java.util.Collection;
 
 /**
